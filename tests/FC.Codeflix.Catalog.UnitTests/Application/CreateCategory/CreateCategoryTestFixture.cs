810a004b69fe0132f3f2b89d04b45aae34dc823a
João Victor Teixeira <48188729+JoaoVictorPereiraTeixeira@users.noreply.github.com>
2022-04-01 22:03:36 -0300

    Implementing data generator

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,74 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Domain.Repository;
+using Moq;
+using System;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture>
+{
+}
+
+public class CreateCategoryTestFixture : BaseFixture
+{
+    public CreateCategoryInput GetInput() => new(
+        GetValidCategoryName(),
+        GetValidCategoryDescription(),
+        GetRandomBoolean()
+    );
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3) categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255) categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000) categoryDescription = categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean() => (new Random()).NextDouble() < 0.5;
+
+    public CreateCategoryInput GetInvalidInputShortName()
+    {
+        var invalidInputShortName = GetInput();
+        invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+        return invalidInputShortName;
+    }
+
+    public CreateCategoryInput GetInvalidInputTooLongName()
+    {
+        var invalidInputTooLongName = GetInput();
+        invalidInputTooLongName.Name = Faker.Lorem.Letter(256);
+        return invalidInputTooLongName;
+    }
+
+    public CreateCategoryInput GetInvalidInputTooLongDescription()
+    {
+        var invalidInputsTooLongDescription = GetInput();
+        invalidInputsTooLongDescription.Description = Faker.Lorem.Letter(10001);
+        return invalidInputsTooLongDescription;
+    }
+
+    public CreateCategoryInput GetInvalidCategoryDescriptionNull()
+    {
+        var invalidInputDescriptionNull = GetInput();
+        invalidInputDescriptionNull.Description = null!;
+        return invalidInputDescriptionNull;
+    }
+
+
+    public Mock<ICategoryRepository> GetRepositoryMock => new();
+    public Mock<IUnitOfWork> GetUnitOfWorkMock => new();
+
+}
