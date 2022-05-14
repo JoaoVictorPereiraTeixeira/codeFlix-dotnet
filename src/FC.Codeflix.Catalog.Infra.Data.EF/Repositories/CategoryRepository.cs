3a0d5a412e9bf092be10dfce0b6e219975685b63
João Victor Teixeira <48188729+JoaoVictorPereiraTeixeira@users.noreply.github.com>
2022-05-14 20:57:08 -0300

    Implementing Pagination

--- src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -33,8 +33,13 @@ namespace FC.Codeflix.Catalog.Infra.Data.EF.Repositories
 
         public async Task<SearchOutput<Category>> Search(SearchInput input, CancellationToken cancellationToken)
         {
+            var toSkip = (input.Page - 1) * input.PerPage;
             var total = await _categories.CountAsync();
-            var items = await _categories.ToListAsync();
+            var items = await _categories.AsNoTracking()
+                .Skip(toSkip)
+                .Take(input.PerPage)
+                .ToListAsync();
+
             return new SearchOutput<Category>(input.Page,input.PerPage, total, items);
         }
 
