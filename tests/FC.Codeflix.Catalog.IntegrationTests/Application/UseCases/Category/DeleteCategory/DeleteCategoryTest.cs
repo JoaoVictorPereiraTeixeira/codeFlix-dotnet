a8d61fef83a0b846c3127bc6f924f7f0da5fb197
João Victor Teixeira <48188729+JoaoVictorPereiraTeixeira@users.noreply.github.com>
2022-05-15 16:30:12 -0300

    Implementing integrated tests - Use Case Delete Category

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,73 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using FluentAssertions;
+using System;
+using FC.Codeflix.Catalog.Application.Exceptions;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTest
+{
+    private readonly DeleteCategoryTestFixture _fixture;
+
+    public DeleteCategoryTest(DeleteCategoryTestFixture fixture) => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DeleteCategory))]
+    [Trait("Integration/Application", "DeleteCategory - Use Cases")]
+    public async Task DeleteCategory()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var categoryExample = _fixture.GetExampleCategory();
+        var exampleList = _fixture.GetExampleCategoriesList(10);
+        await dbContext.AddRangeAsync(exampleList);
+        var trakingInfo = await dbContext.AddAsync(categoryExample);
+        await dbContext.SaveChangesAsync();
+        trakingInfo.State = EntityState.Detached;
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+
+        var input = new DeleteCategoryInput(categoryExample.Id);
+        var useCase = new UseCase.DeleteCategory(
+            repository,
+            unitOfWork
+        );
+
+        await useCase.Handle(input, CancellationToken.None);
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var dbCategoryDeleted = await assertDbContext.Categories.FindAsync(categoryExample.Id);
+        var dbCategories = await assertDbContext.Categories.ToListAsync();
+
+        dbCategoryDeleted.Should().BeNull();
+        dbCategories.Should().HaveCount(exampleList.Count);
+    }
+
+    [Fact(DisplayName = nameof(DeleteCategoryThrowsWhenNotFound))]
+    [Trait("Integration/Application", "DeleteCategory - Use Cases")]
+    public async Task DeleteCategoryThrowsWhenNotFound()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var exampleList = _fixture.GetExampleCategoriesList(10);
+        await dbContext.AddRangeAsync(exampleList);
+        await dbContext.SaveChangesAsync();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+
+        var input = new DeleteCategoryInput(Guid.NewGuid());
+        var useCase = new UseCase.DeleteCategory(
+            repository,
+            unitOfWork
+        );
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+        await task.Should()
+            .ThrowAsync<NotFoundException>()
+            .WithMessage($"category '{input.Id}' not found.");
+    }
+}
